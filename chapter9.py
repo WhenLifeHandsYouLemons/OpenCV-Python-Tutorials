e6397affc15c0d7ea022bee39439cff48d8aeede
Sooraj Sannabhadti <You'veGotToMakeLemonade@users.noreply.github.com>
2020-11-09 21:41:31 +0800

    Working face detection

--- chapter9.py
+++ chapter9.py
@@ -2,11 +2,11 @@
 Chapter 9: Face Detection
 """
 import cv2
-import numpy as np
 
 faceCascade = cv2.CascadeClassifier("Resources/Haarcascades/haarcascade_frontalface_default.xml")
 
-img = cv2.imread("Resources/lena.jpg")
+img = cv2.imread("Resources/face.jpg")
+img = cv2.resize(img, (512, 512))
 cv2.imshow("Result", img)
 
 imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,5 +16,9 @@ faces = faceCascade.detectMultiScale(imgGrey, 1.1, 4)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
+cv2.imshow("Result", img)
 
 cv2.waitKey(0)
+"""
+Press 'Q' to exit examples
+"""
